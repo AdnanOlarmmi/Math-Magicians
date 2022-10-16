a6888d2d315638b11214c06b8000f58aa61fecca
AdnanOlarmmi <olarmmi@gmail.com>
2022-10-16 15:11:21 +0100

    App tweak

--- src/index.js
+++ src/index.js
@@ -2,23 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
+const Square = (props) => {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={() => {
+            props.onClick();
+        }}>
+          {props.value}
         </button>
       );
-    }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+
+        if(calculateWinner(squares)) {
+            return;
+        }
+        squares[i] =  this.state.xIsNext? 'X' : 'O';
+        this.setState({
+            squares,
+            xIsNext: !this.state.xIsNext,
+        });
+        
+    }
+
     renderSquare(i) {
-      return <Square />;
+      return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
     }
   
     render() {
-      const status = 'Next player: X';
+
+      const winner = calculateWinner(this.state.squares);
+      let status; 
+      if(winner) {
+       status =  `Winner: ${winner}`
+      } else {
+        status =  `Next player: ${this.state.xIsNext? 'X': 'O'}`
+      }
+      
   
       return (
         <div>
@@ -59,6 +89,26 @@ class Square extends React.Component {
     }
   }
   
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   // ========================================
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
