44c9342ce0f53ebbdcdcc4459c175bb37d447569
AdnanOlarmmi <olarmmi@gmail.com>
2022-10-24 12:20:06 +0100

    Create Set up for Math-Magician Project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 
- ReactDOM.render(<h1>Hello React</h1>, document.getElementById("root"));
-  
+ReactDOM.render(<h1>Hello React</h1>, document.getElementById('root'));
