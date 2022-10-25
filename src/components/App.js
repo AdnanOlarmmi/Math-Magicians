7c061fc73881120c4fafc8804ea656b1065b1a59
AdnanOlarmmi <olarmmi@gmail.com>
2022-10-25 08:54:20 +0100

    Import Calculator Component into App Component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Calculator from './Calculator';
+
+// eslint-disable-next-line
+class App extends Component {
+  render() {
+    return (<div><Calculator /></div>);
+  }
+}
+
+export default App;
