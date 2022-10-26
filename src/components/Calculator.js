c49787dc8c41bccee28603306fa4abf7688a4bd2
AdnanOlarmmi <olarmmi@gmail.com>
2022-10-26 21:53:32 +0100

    Refactor Calculator Component to Functional

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,28 +1,20 @@
 /* eslint-disable */
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import calculate from '../logic/calculate';
 
 // eslint-disable-next-line
-class Calculator extends Component {
+const Calculator = () => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+  const [ state, setState ] = useState({ total:null, next:null, operation: null });
+
+  const { total, next, operation } = state;
+
+  const calculateValues = (e) => {
+    const calcButton = e.target.innerHTML;
+    const values = state;
+    setState(calculate(values, calcButton));
   }
 
-  calculateValues(e) {
-    this.calculatorCells = e.target.innerHTML;
-    const calcButtons = this.calculatorCells;
-    const values = this.state;
-    this.setState(calculate(values, calcButtons));
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
     return (
       <div className="calculator">
         <div className="output">
@@ -30,38 +22,37 @@ class Calculator extends Component {
         </div>
         <table className="input" cellSpacing={0}>
           <tr>
-            <td onClick={this.calculateValues.bind(this)}>AC</td>
-            <td onClick={this.calculateValues.bind(this)}>+/-</td>
-            <td onClick={this.calculateValues.bind(this)}>%</td>
-            <td onClick={this.calculateValues.bind(this)}>÷</td>
+            <td onClick={calculateValues}>AC</td>
+            <td onClick={calculateValues}>+/-</td>
+            <td onClick={calculateValues}>%</td>
+            <td onClick={calculateValues}>÷</td>
           </tr>
           <tr>
-            <td onClick={this.calculateValues.bind(this)}>7</td>
-            <td onClick={this.calculateValues.bind(this)}>8</td>
-            <td onClick={this.calculateValues.bind(this)}>9</td>
-            <td onClick={this.calculateValues.bind(this)}>x</td>
+            <td onClick={calculateValues}>7</td>
+            <td onClick={calculateValues}>8</td>
+            <td onClick={calculateValues}>9</td>
+            <td onClick={calculateValues}>x</td>
           </tr>
           <tr>
-            <td onClick={this.calculateValues.bind(this)}>4</td>
-            <td onClick={this.calculateValues.bind(this)}>5</td>
-            <td onClick={this.calculateValues.bind(this)}>6</td>
-            <td onClick={this.calculateValues.bind(this)}>-</td>
+            <td onClick={calculateValues}>4</td>
+            <td onClick={calculateValues}>5</td>
+            <td onClick={calculateValues}>6</td>
+            <td onClick={calculateValues}>-</td>
           </tr>
           <tr>
-            <td onClick={this.calculateValues.bind(this)}>1</td>
-            <td onClick={this.calculateValues.bind(this)}>2</td>
-            <td onClick={this.calculateValues.bind(this)}>3</td>
-            <td onClick={this.calculateValues.bind(this)}>+</td>
+            <td onClick={calculateValues}>1</td>
+            <td onClick={calculateValues}>2</td>
+            <td onClick={calculateValues}>3</td>
+            <td onClick={calculateValues}>+</td>
           </tr>
           <tr>
-            <td colSpan={2} onClick={this.calculateValues.bind(this)}>0</td>
-            <td onClick={this.calculateValues.bind(this)}>.</td>
-            <td onClick={this.calculateValues.bind(this)}>=</td>
+            <td colSpan={2} onClick={calculateValues}>0</td>
+            <td onClick={calculateValues}>.</td>
+            <td onClick={calculateValues}>=</td>
           </tr>
         </table>
       </div>
     );
-  }
 }
 
 export default Calculator;
