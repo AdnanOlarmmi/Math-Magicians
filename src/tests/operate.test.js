3ede7f6788006c89db6df62ee1306b8a874f8bd2
AdnanOlarmmi <olarmmi@gmail.com>
2022-11-03 14:41:50 +0100

    Add better description

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,16 +1,16 @@
 import operate from "../logic/operate";
 
 describe("Test operate", () => {
-    test('adds 1 + 2 to equal 3', () => {
+    test('add 1 and 2 to equal 3', () => {
     expect(operate(1, 2, '+')).toBe("3");
   });
-  test('adds 1 + 2 to equal 3', () => {
+    test('subtract 1 from 5 to equal 4', () => {
     expect(operate(5, 1, '-')).toBe("4");
   });
-  test('adds 1 + 2 to equal 3', () => {
+    test('multiply 5 by 5 to equal 25', () => {
     expect(operate(5, 5, 'x')).toBe("25");
   });
-  test('adds 1 + 2 to equal 3', () => {
+    test('divide 16 by 4 to equal 4', () => {
     expect(operate(16, 4, '÷')).toBe("4");
   });
 })
